937a1faa26d01cb92c203c8289ad0d5c220f5336
Aju-Dev <152674096+Aju-Dev@users.noreply.github.com>
2024-08-28 10:56:22 +0530

    file uploaded

---
+++ main.js
@@ -0,0 +1,40 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2500,
+    delay: 300,
+   /* reset: true*/
+})
+
+/*sr.reveal('h3,h1')
+sr.reveal('p', {delay:1200, interval: 100})
+sr.reveal('.icons', {delay:2000, interval: 100})
+sr.reveal('.name', {delay:1200, interval: 100})
+sr.reveal('h4', {delay:800, interval: 100})*/
+
+ScrollReveal().reveal('.h33',{origin:'left'});
+
+/*ScrollReveal().reveal('.py', { delay: 200});
+ScrollReveal().reveal('.sql', { delay: 400});
+ScrollReveal().reveal('.html', { delay: 600});
+ScrollReveal().reveal('.css', { delay: 800});
+ScrollReveal().reveal('.js', { delay: 1000});
+ScrollReveal().reveal('.bs', { delay: 1200});
+ScrollReveal().reveal('.dj', { delay: 1400});
+ScrollReveal().reveal('.react', { delay: 1600});*/
+
+
+ScrollReveal().reveal('.one',{origin:'left'});
+ScrollReveal().reveal('.two',{delay:1000,origin:'left'});
+ScrollReveal().reveal('.three',{delay:1500,origin:'left'});
+ScrollReveal().reveal('.four',{origin:'left'});
+
+ScrollReveal().reveal('.animate',{origin:'left',delay:1500});
+ScrollReveal().reveal('.animate2',{origin:'left',delay:2000});
+ScrollReveal().reveal('h1',{origin:'bottom'});
+ScrollReveal().reveal('.firstp, .abt-p', { delay: 1000});
+ScrollReveal().reveal('.icons', { delay: 2000,origin:'top' });
+ScrollReveal().reveal('.contact-box', { delay: 2000 });
+ScrollReveal().reveal('.sk-h5', { delay: 1200});
+ScrollReveal().reveal('.name', { delay: 1200 });
+
